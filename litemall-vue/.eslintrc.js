e4b19dbb52b10484c461c6b16aeb550ce8580e56
“matianheng” <1157838984@qq.com>
2024-09-10 15:00:51 +0800

    添加抖音项目

--- litemall-vue/.eslintrc.js
+++ litemall-vue/.eslintrc.js
@@ -12,6 +12,12 @@ module.exports = {
   //   'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   // },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
+    parser: 'babel-eslint',  // 使用 Babel 解析器
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:vue/essential',  // 支持 Vue 文件
+    'eslint:recommended',
+  ],
 };
